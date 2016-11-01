872578d85fdcdc11daa27717fe561f6a74563d98
ankit16-19 <ankitsocial16@gmail.com>
2016-11-01 11:11:46 +0530

    Update multiplying 2 matrices using DMA.c

--- multiplying 2 matrices using DMA.c
+++ multiplying 2 matrices using DMA.c
@@ -1,9 +1,6 @@
-
-//oye!!!!
-//not saving the values of matrix 1 and 2 skipping the for loop and exiting the programme
-
 //multiplying 2 matrices using D.M.A
 #include<stdio.h>
+#include<stdlib.h>
 main()
 {
     int *a[10],*b[10],*c[10],row1,col1,row2,col2;
@@ -23,30 +20,54 @@ main()
     printf("Enter the values of matrix 1 row wise:\n");
     for(i=0;i<row1;i++)
     {
-        for(o=o;o<col1;o++)
+        for(o=0;o<col1;o++)
             scanf("%d",(*(a+i)+o));
     }
     for(i=0;i<row1;i++)
     {
-        for(o=o;o<col1;o++)
-            printf("%d",*(*(a+i)+o));
+           for(o=0;o<col1;o++)
+            {
+                printf("%d ",*(*(a+i)+o));
+            }
+            printf("\n");
+
     }
     printf("Enter he values of matrix 2 row wise:\n");
     for(i=0;i<row2;i++)
     {
-        for(o=o;o<col2;o++)
+        for(o=0;o<col2;o++)
             scanf("%d",(*(b+i)+o));
     }
     for(i=0;i<row2;i++)
     {
-        for(o=o;o<col2;o++)
-        printf("%d",*(*(b+i)+o));
+        for(o=0;o<col2;o++)
+         {
+             printf("%d ",*(*(b+i)+o));
+         }
+         printf("\n");
     }
-
-
-
-
-
-
-
+    for(i=0;i<row1;i++)
+    {
+        c[i] = (int *)calloc(col2,sizeof(int));
+    }
+    for(i=0;i<row1;i++)
+    {
+        for(o=0;o<col2;o++)
+        {
+            int k=0,l=0;
+            while(k<col2 && l< row1)
+            {*(*(c+i)+o) += (*(*(a+i)+k))*(*(*(b+l)+o));
+            k++;
+            l++;}
+        }
+   }
+    printf("Multiplied  matrix is:\n");
+    for(i=0;i<row1;i++)
+    {
+        for(o=0;o<col2;o++)
+         {
+             printf("%d ",*(*(c+i)+o));
+         }
+         printf("\n");
+    }
 }
