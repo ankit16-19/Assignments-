b994fd72312d39860bb6b7b2d506e5e15a6d4488
ankit16-19 <ankitsocial16@gmail.com>
2016-11-01 10:27:27 +0530

    Update multiplying 2 matrices using DMA.c

--- multiplying 2 matrices using DMA.c
+++ multiplying 2 matrices using DMA.c
@@ -2,7 +2,6 @@
 //oye!!!!
 //not saving the values of matrix 1 and 2 skipping the for loop and exiting the programme
 
-
 //multiplying 2 matrices using D.M.A
 #include<stdio.h>
 main()
@@ -25,23 +24,29 @@ main()
     for(i=0;i<row1;i++)
     {
         for(o=o;o<col1;o++)
-            scanf("%d",*(a+i)+o);
+            scanf("%d",(*(a+i)+o));
     }
     for(i=0;i<row1;i++)
     {
         for(o=o;o<col1;o++)
-            printf("%d",*(a+i)+o);
+            printf("%d",*(*(a+i)+o));
     }
     printf("Enter he values of matrix 2 row wise:\n");
     for(i=0;i<row2;i++)
     {
         for(o=o;o<col2;o++)
-            scanf("%d",*(b+i)+o);
+            scanf("%d",(*(b+i)+o));
     }
     for(i=0;i<row2;i++)
     {
         for(o=o;o<col2;o++)
-        printf("%d",*(b+i)+o);
+        printf("%d",*(*(b+i)+o));
     }
+
+
+
+
+
+
 
 }
